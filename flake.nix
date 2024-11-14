5a7267fe3d9d52f04c9af42c264f91a406f89f46
Christina O'Donnell <cdo@mutix.org>
2024-11-14 11:09:19 +0000

    Implement load()

--- flake.nix
+++ flake.nix
@@ -22,6 +22,7 @@
               seaborn
               scipy
               nbdime
+              xlrd
             ]))
             git
           ];
--- preprocess.py
+++ preprocess.py
@@ -1,28 +1,38 @@
+import pandas as pd
+import numpy as np
+import scipy
+import sklearn
+
 base_df = None
 
 # Load returns the dataframe frm the loaded data.
 # This sets self.base_df to a this dataframe and returns it.
 def load(filename):
-    pass
+    raw_df = pd.read_excel(filename)
+    return raw_df
 
 # Take a data-frame and replace 999 with the mean value for each numerical column.
 # Returns an imputed data-frame.
-def impute_mean():
+def impute_mean(df):
     pass
 
 # Take a data-frame and replace 999 with the median value for each numerical column.
 # Returns an imputed data-frame.
-def impute_median():
+def impute_median(df):
     pass
 
 #TODO: More imputation methods?
 
-def normalize():
+def normalize(df):
     pass
 
-def one_hot_encode():
+def one_hot_encode(df):
     pass
 
 # Utility function to get pre-processed data.
-def preprcess(df):
+def preprocess():
+    df = load()
+
+# Returns a numpy array from a data-frame.
+def as_numpy(df):
     pass
