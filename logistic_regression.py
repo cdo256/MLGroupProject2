08a267f4823dfcdb7361544d55adad376b8d5268
Christina O'Donnell <cdo@mutix.org>
2024-11-22 10:06:48 +0000

    Fix more basic errors in logistic regression

--- logistic_regression.py
+++ logistic_regression.py
@@ -1,11 +1,11 @@
 from model import *
 import sklearn.linear_model
 
-class LinearRegression(ClassificationModel):
+class LogisticRegression(ClassificationModel):
     def __init__(self, **hyperparams):
         self.param_grid = {}
         self.hyperparams = hyperparams
-        self.model = sklearn.linear_model.LinearRegression(**hyperparams)
+        self.model = sklearn.linear_model.LogisticRegression(**hyperparams)
     
     def param_search(self, X, y):
         self.model, self.hyperparams = sklearn_param_search(
