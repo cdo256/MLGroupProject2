53ff66f6ddf89e685c594b3fc85d3facfe83bc11
Christina O'Donnell <cdo@mutix.org>
2024-12-12 14:43:53 +0000

    Add ElasticNetRegressor.

--- linear_regression.py
+++ linear_regression.py
@@ -8,8 +8,35 @@ class LinearRegression(RegressionModel):
         self.hyperparams = hyperparams
         self.model = sklearn.linear_model.LinearRegression(**hyperparams)
     
+    def param_search(self, x, y):
+        pass # no hyperparams to tune
+
+    def train(self, x, y):
+        self.model.fit(x, y)
+        return self.model.score(x, y)
+
+    def test(self, x, y):
+        return self.model.score(x, y)
+    
+
+    def predict(self,x):
+        return self.model.predict(x)
+        print(f"prediction: {self.model.predict(x)}")
+        print(f"prediction probability: {self.model.predict_proba}")
+
+class ElasticNetRegressor(RegressionModel):
+    def __init__(self, **hyperparams):
+        self.name = 'ElasticNetRegressor'
+        self.param_grid = {
+            'alpha': [5000e-3, 1000e-3, 500e-3, 200e-3, 100e-3, 10e-3, 1e-3, 100e-6, 10e-6],
+            'l1_ratio': [0.0, 0.1, 0.3, 0.5, 0.7, 0.9, 1.0],
+        }
+        self.hyperparams = hyperparams
+        self.model = sklearn.linear_model.ElasticNet(**hyperparams)
+    
     def param_search(self, X, y):
-        pass # No hyperparams to tune
+        self.hyperparams = sklearn_param_search(
+            self.model, self.param_grid, X, y)
 
     def train(self, X, y):
         self.model.fit(X, y)
